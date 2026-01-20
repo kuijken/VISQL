8b2ee0d9d307c25258668c23971f92276279c9e3
kuijken <kuijken@strw.leidenuniv.nl>
2026-01-20 13:40:30 +0100

    General update of scripts to bring in more in line with what runs in Leiden Jan 2026

--- cronjobs/psf/medianmoments.py
+++ cronjobs/psf/medianmoments.py
@@ -1,13 +1,41 @@
 import numpy as np
 from sys import argv
+from astropy.table import Table
 
 print('#|   catalogue                                                 |'+
-          '      R2  |    COMA1 |  COMA 2 |    ELL 1 |   ELL 2 |'+
-          '   TREF 1 |  TREF 2 |')
+          '      R2  |    COMA1 |   COMA 2 |    ELL 1 |    ELL 2 |'+
+          '   TREF 1 |   TREF 2 |')
+
 for momcat in argv[1:]:
-    r2,e1,e2,t1,t2,c1,c2=np.loadtxt(momcat,unpack=True,
-                                        usecols=[12,13,14,15,16,17,18])
-    print('| %-60s | %8.3f | %8.4f |%8.4f | %8.4f |%8.4f | %8.4f |%8.4f |'
+    try:
+        t=Table.read(momcat,format='ascii')
+        r2=t['R2']
+        e1=t['e1']
+        e2=t['e2']
+        t1=t['tr1']
+        t2=t['tr2']
+        c1=t['com1']
+        c2=t['com2']
+        if len(r2)==0:
+            r2,e1,e2,t1,t2,c1,c2=[9.9], [0.9999], [0.9999], [0.9999], [0.9999], [0.9999], [0.9999]
+    except:
+        r2,e1,e2,t1,t2,c1,c2=[9.9], [0.9999], [0.9999], [0.9999], [0.9999], [0.9999], [0.9999]
+
+    print('| %-60s | %8.3f | %8.4f | %8.4f | %8.4f | %8.4f | %8.4f | %8.4f |'
               % tuple([momcat]+[np.median(x) for x in [r2,c1,c2,e1,e2,t1,t2]]),
               )
+
+
+
+
+
+
+
+
+# for momcat in argv[1:]:
+#     try:
+#         r2,e1,e2,t1,t2,c1,c2=np.loadtxt(momcat,unpack=True,
+#                                         usecols=[12,13,14,15,16,17,18])
+#     except:
+#         r2,e1,e2,t1,t2,c1,c2=[99], [0.9999], [0.9999], [0.9999], [0.9999], [0.9999], [0.9999]
 
--- cronjobs/psf/plotpsftimeseries.py
+++ cronjobs/psf/plotpsftimeseries.py
@@ -31,7 +31,17 @@ missionday=np.array([
     for n in imname])
 day0=int(missionday[0])
 dayf=int(missionday[-1])+1
-
+dayf=day0+31
+year0=int(imname[0][2:6])
+month0=int(imname[0][6:8])
+if month0 < 12:
+    yearf=year0+0
+    monthf=month0+1
+else:
+    yearf=year0+1
+    monthf=1
+day0=(datetime.date(year0,month0,1)-datetime.date(2023,7,1)      ).total_seconds()/86400.
+dayf=(datetime.date(yearf,monthf,1)-datetime.date(2023,7,1)      ).total_seconds()/86400.
 def mm(v,v1,v2):
     return np.minimum(np.maximum(v,v1),v2)
 
@@ -79,5 +89,86 @@ plt.colorbar(label='Texp',aspect=10)
 plt.xlabel('Mission day ('+imname[0][2:17]+' to '+imname[-1][2:17]+')')
 plt.tight_layout()
 plt.savefig('IQ_timeline_'+yyyymm+'.png')
+plt.clf()
 
+# now repeat the plot, but plot last 31 days instead of current month as above
+
+# first read in the previous month
+
+y,m=int(yyyymm[:4]),int(yyyymm[4:6])
+if (m==1):
+    y-=1
+    m=12
+else:
+    m-=1
+yyyymm2='%04i%02d' % (y,m)
+f2=Table.read('psfmoments_'+yyyymm2+'.txt',format='ascii.commented_header')
+
+imname2=f2['catalogue']
+texp2=[exptimes['EXPTIME'][exptimes['FITSNAME']==im[:-15]+".fits"][0] for im in imname2]
+
+missionday2=np.array([
+    (datetime.datetime(
+        int(n[2:6]),int(n[6:8]),int(n[8:10]),
+        int(n[11:13]),int(n[13:15]),int(n[15:17])) -
+        datetime.datetime(2023,7,1,0,0,0)).total_seconds()/86400.
+    for n in imname2])
+
+dayf=int(missionday[-1])+1
+day0=dayf-31
+date0=imname[-1][2:10]+'_000000'
+y,m,d=int(date0[:4]),int(date0[4:6]),int(date0[6:8])
+date0=datetime.date(y,m,d)-datetime.timedelta(days=30)
+date0='%04d%02d%02d_000000' % (date0.year,date0.month,date0.day)
+
+plt.figure(figsize=(14,12))
+plt.subplot(511)
+plt.scatter(missionday,mm(f['R2'],1.72,2.28),c=texp,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.scatter(missionday2,mm(f2['R2'],1.72,2.28),c=texp2,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.ylim(1.7,2.3)
+plt.xlim(day0,dayf)
+plt.xticks(np.arange(day0,dayf+1,1))
+plt.ylabel('R2')
+plt.grid()
+plt.colorbar(label='Texp',aspect=10)
+plt.subplot(512)
+plt.scatter(missionday,mm(f['ELL 1'],-0.065,0.065),c=texp,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.scatter(missionday2,mm(f2['ELL 1'],-0.065,0.065),c=texp2,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.ylim(-0.07,0.07)
+plt.xlim(day0,dayf)
+plt.xticks(np.arange(day0,dayf+1,1))
+plt.ylabel('e1')
+plt.grid()
+plt.colorbar(label='Texp',aspect=10)
+plt.subplot(513)
+plt.scatter(missionday,mm(f['ELL 2'],-0.065,0.065),c=texp,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.scatter(missionday2,mm(f2['ELL 2'],-0.065,0.065),c=texp2,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.ylim(-0.07,0.07)
+plt.xlim(day0,dayf)
+plt.xticks(np.arange(day0,dayf+1,1))
+plt.ylabel('e2')
+plt.grid()
+plt.colorbar(label='Texp',aspect=10)
+plt.subplot(514)
+plt.scatter(missionday,mm(f['TREF 1'],-0.29,-0.16),c=texp,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.scatter(missionday2,mm(f2['TREF 1'],-0.29,-0.16),c=texp2,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.ylim(-0.3,-0.15)
+plt.xlim(day0,dayf)
+plt.xticks(np.arange(day0,dayf+1,1))
+plt.ylabel('t1')
+plt.grid()
+plt.colorbar(label='Texp',aspect=10)
+plt.subplot(515)
+plt.scatter(missionday,mm(f['TREF 2'],-0.04,0.09),c=texp,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.scatter(missionday2,mm(f2['TREF 2'],-0.04,0.09),c=texp2,vmin=0,vmax=700,s=1,cmap='turbo')
+plt.ylim(-0.05,0.1)
+plt.xlim(day0,dayf)
+plt.xticks(np.arange(day0,dayf+1,1))
+plt.ylabel('t2')
+plt.grid()
+plt.colorbar(label='Texp',aspect=10)
+plt.xlabel('Mission day ('+date0+' to '+imname[-1][2:17]+')')
+plt.tight_layout()
+plt.savefig('IQ_timeline_lastmonth.png')
+plt.clf()
 
